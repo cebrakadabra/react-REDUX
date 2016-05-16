f9afdda56a47aef6003e97eb3c420bf6de5fdc1f
Christoph Eberl <office@cebra-webdesign.at>
2016-05-16 12:15:24 -0700

    refactored actions

--- src/js/actions/ProductActions.js
+++ src/js/actions/ProductActions.js
@@ -1,38 +1,45 @@
-
-import dispatcher from "../dispatcher";
-
-export function createProduct(text) {
-  dispatcher.dispatch({
-    type: "CREATE_PRODUCT",
-    text,
-  });
-}
-
-export function deleteProduct(id) {
-  dispatcher.dispatch({
-    type: "DELETE_PRODUCT",
-    id,
-  });
-}
-
-export function reloadProducts() {
-  dispatcher.dispatch({type: "FETCH_PRODUCTS"});
-  setTimeout(() => {
-    dispatcher.dispatch({type: "RECEIVE_PRODUCTS", products: [
-      {
-        id: 8484848484,
-        text: "TV Series One",
-        live: false
-      },
-      {
-        id: 6262627272,
-        text: "TV Series Two",
-        live: true
-      },
-    ]});
-
-    if(false) {
-      dispatcher.dispatch({type: "FETCH_PRODUCTS_ERROR"});
+let actions = {
+  createProduct: function(text) {
+    return {
+      type: "CREATE_PRODUCT",
+      text: text
     }
-  }, 1000);
+  },
+
+  deleteProduct: function(id) {
+    return {
+      type: "DELETE_PRODUCT",
+      id: id
+    }
+  },
+
+
 }
+
+export default actions
+
+
+
+// TODO refactor reloadProducts function
+
+// export function reloadProducts() {
+//   dispatcher.dispatch({type: "FETCH_PRODUCTS"});
+//   setTimeout(() => {
+//     dispatcher.dispatch({type: "RECEIVE_PRODUCTS", products: [
+//       {
+//         id: 8484848484,
+//         text: "TV Series One",
+//         live: false
+//       },
+//       {
+//         id: 6262627272,
+//         text: "TV Series Two",
+//         live: true
+//       },
+//     ]});
+//
+//     if(false) {
+//       dispatcher.dispatch({type: "FETCH_PRODUCTS_ERROR"});
+//     }
+//   }, 1000);
+// }
